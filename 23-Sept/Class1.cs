ae3be0a830df123cf7ca64230a03c1fa2cda98f4
UtkarN <95558826+UtkarN@users.noreply.github.com>
2022-09-29 10:49:41 +0530

    Add files via upload

---
+++ 23-Sept/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace LambdaExpression
+{
+
+
+    public delegate int greater(int a,int b);
+    // multicast 
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            greater obj = (p,q) => large=(p>q)?p:q;
+
+
+            Console.WriteLine("Enter a number1");
+            int a= int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a number2");
+            int b= int.Parse(Console.ReadLine());
+
+            Console.WriteLine(obj.Invoke(a,b)); ;
+
+
+
+        }
+    }
+}
